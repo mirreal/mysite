0c618a9e96a06bddb831163979b3c8e4d82851dc
mirreal <zgkm181142521@gmail.com>
2014-05-02 21:03:25 +0800

    chatroom

---
+++ app.js
@@ -0,0 +1,90 @@
+﻿var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({server : server});
+
+var clientsList = [];
+var colors = ["#C03", "DarkKhaki", "yellow", "grey", "pink",
+  "green", "orange", "brown", "Magenta", "DeepSkyBlue", 'purple', 'plum'];
+
+colors.sort(function(a,b) {
+  return Math.random() > 0.5;
+});
+
+
+wss.on('connection', function(client) {
+  clientsList.push(client);
+  var userName = false;
+  var userColor = false;
+
+  client.on('message', function(msg) {
+    if(!userName) {
+        userName = msg;
+        userColor = colors.shift();
+        client.send(JSON.stringify({
+          type:'color',
+          data: userColor
+        }));
+
+        console.log(userName + ' login');
+    } else {
+        console.log(userName + ' say: ' + msg);
+
+        var obj = {
+          time: (new Date()).getTime(),
+          text: msg,
+          author: userName,
+          color: userColor
+        };
+
+        var json = JSON.stringify({
+          type:'message',
+          data: obj
+        });
+
+        for (var i=0; i < clientsList.length; i++) {
+          if (client !== clientsList[i]) {
+            clientsList[i].send(json);
+          }
+          //clientsList[i].send(json);
+        }
+    }
+  });
+
+  client.on('close', function() {
+    var index = clientsList.indexOf(client);
+    clientsList.splice(index, 1);
+    if(userName != false && userColor != false) {
+      colors.push(userColor);
+    }  
+  });
+  
+});
+
+// all environments
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  res.sendfile('views/chat.html');
+});
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ public/js/client.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+    var content = $('#content');
+    var status = $('#status');
+    var input = $('#input');
+
+    var myColor = false;
+    var myName = false;
+
+    //var ws = new WebSocket('ws:' + window.location.href.substring(window.location.protocol.length));
+    var ws = new WebSocket('ws:127.0.0.1:3000');
+
+    ws.onopen = function () {
+        status.text('username:');
+    };
+
+    ws.onmessage = function (message) {
+        var json = JSON.parse(message.data);
+
+        if (json.type === 'color') {
+            myColor = json.data;
+            status.text(myName + ': ').css('color', myColor);
+        } else if (json.type === 'message') {
+            addMessage(json.data.author, json.data.text, json.data.color, new Date(json.data.time));
+        } 
+    };
+
+    input.keydown(function(e) {
+
+        if (e.keyCode === 13) {
+            var msg = $(this).val();
+            if (!msg) return;
+            ws.send(msg);
+            $(this).val('');
+            if (myName === false) {
+                myName = msg;
+            } else {
+                addMessageToDOM(msg);
+            }
+
+        }
+    });
+
+
+    function addMessage(author, message, color, date) {
+        content.append('<div><img src="img/vincent.jpg" width="32"> </img>'
+            + '<p class="name" style="color:' + color + '">' + author + '</p> '
+            + '<p class="message">'
+            + ' ---- ' + message + '</p>' + '<p class="time">' + date.getHours() + ':'
+            + (date.getMinutes()<10 ? ('0'+ date.getMinutes()) : date.getMinutes()) + ':'
+            + (date.getSeconds()<10 ? ('0' + date.getSeconds()) : date.getSeconds()) + '</p></div>'
+        );
+    }
+
+    function addMessageToDOM(msg) {
+        var date = new Date();
+        content.append('<div><img src="img/vincent.jpg" width="32"> </img>'
+            + '<p style="color:' + myColor + '">' + myName + '</p> '
+            + '<p">'
+            + ' ---- ' + msg + '</p>' + '<p class="time">' + date.getHours() + ':'
+            + (date.getMinutes()<10 ? ('0'+ date.getMinutes()) : date.getMinutes()) + ':'
+            + (date.getSeconds()<10 ? ('0' + date.getSeconds()) : date.getSeconds()) + '</p></div>')
+    }
+});
